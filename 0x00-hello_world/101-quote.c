a6f617b137b2c4cf1691da07a4d4016aa406896a
Samiratou Bonkoungou <bonkoungousamiratou75@gmail.com>
2021-08-26 13:12:31 -0700

    update: 101-quote.c C program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,6 +13,8 @@ int main(void)
 	
 	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
 
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
 	return (1);
 }
